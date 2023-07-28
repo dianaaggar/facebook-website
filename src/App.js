f454bfc806f97cf8f3e61ec7a3570acb9d2fb25c
Shehroze4543 <86188771+Shehroze4543@users.noreply.github.com>
2023-07-27 21:56:45 -0400

    shehroze' commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <nav>
+        <h1>HELLO DIANA</h1>
         <div class="navBar-left">
-        <img src={"/facebookLogo.png"} alt="" class="logo" />
+          <img src={"/facebookLogo.png"} alt="" class="logo" />
           <ul>
-            <li><img src='' alt=""/> </li>
-            <li><img src='' alt=""/> </li>
-            <li><img src='' alt=""/> </li>
-            <li><img src='' alt=""/> </li>
+            <li>
+              <img src="" alt="" />{" "}
+            </li>
+            <li>
+              <img src="" alt="" />{" "}
+            </li>
+            <li>
+              <img src="" alt="" />{" "}
+            </li>
+            <li>
+              <img src="" alt="" />{" "}
+            </li>
           </ul>
         </div>
-        <div class="navBar-right"> 
-        <div class="searchBox">
-         </div>
-         </div>
+        <div class="navBar-right">
+          <div class="searchBox"></div>
+        </div>
       </nav>
 
-      <div class ="container">
+      <div class="container">
         <div class="leftcontainer"></div>
         <div class="rightcontainer"></div>
-
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default App;
