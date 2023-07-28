2810b7d149b3fe8936135fcfda19fdbd9d339217
dianaaggar <57739072+dianaaggar@users.noreply.github.com>
2023-07-28 12:55:35 -0400

    added new story

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ import events from './images/events.png';
 import group from './images/group.png';
 import pages from './images/pages.png';
 import upload from './images/upload.png';
-import southpark from './images/southpark.jpg'
+import southpark from './images/southpark.jpg';
+import photo from './images/photo.png';
+import feeling from './images/feeling.png';
+import kenny from './images/kenny.png';
+import kely from './images/kely.png';
+import stan from './images/stan.png';
+import wendy from './images/wendy.png';
 
 function App() {
   return ( 
@@ -61,31 +67,52 @@ function App() {
 
         <div className="maincontainer">
           <div className='maincont'>
-          <div className='story'>
+          <div className='story story1'>
           <img src={upload} alt="" />
           <p> Post Story</p>
           </div>
 
-          <div className='story'>
-          <img src={upload} alt="" />
-          <p> Post Story</p>
+          <div className='story story2'>
+          <img src={stan} alt="" />
+          <p> Stan</p>
           </div>
 
-          <div className='story'>
-          <img src={upload} alt="" />
-          <p> Post Story</p>
+          <div className='story story3'>
+          <img src={kenny} alt="" />
+          <p> Kenny</p>
           </div>
 
-          <div className='story'>
-          <img src={upload} alt="" />
-          <p> Post Story</p>
+          <div className='story story4'>
+          <img src={kely} alt="" />
+          <p> Kely</p>
           </div>
-          <div className='story'>
-          <img src={upload} alt="" />
-          <p> Post Story</p>
+
+          <div className='story story5'>
+          <img src={wendy} alt="" />
+          <p> Wendy</p>
+          </div>
+          </div>
+
+          <div className="postContainer">
+            <div className="userProfile">
+            <img src={cartman} alt="" />
+            
+            <p>Cartman South Park</p>
+            <span>Public</span>
+            </div>
+          <div className="inputContainer">  
+          <input type="text" placeholder='What is on your mind, Cartman?'></input>
+
+          <div className='postLinks'>
+            <a href="#"><img src={livevideo} alt=""/> Live Video</a>
+            <a href="#"><img src={photo} alt=""/> Photo/Video</a>
+            <a href="#"><img src={feeling} alt=""/> Feeling/Activity</a>
           </div>
 
           </div>
+            </div>
+
+          
         </div>
 
 
